33f9ff3637fb245e200af44efe36b3957df17024
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2017-11-12 14:31:55 -0500

    Checks if two strings are the same

---
+++ check_strings.py
@@ -0,0 +1,21 @@
+# Created by : Tochukwu Iroakazi
+# Created on : Nov-2017
+# Created for : ICS3UR-1 
+# Daily Assignment - Unit4-06
+# This program checks if two atrings are the same 
+
+
+def get_string(boolean):
+    
+    first = string1.upper()
+    second = string2.upper()
+    if first == second:
+      print('true')
+    else:
+      print('false')
+    
+string1 = raw_input('Type in the first string : ')
+string2 = raw_input('Type in the second string : ')
+boolean = get_string(string1)
+boolean = get_string(string2)
+
